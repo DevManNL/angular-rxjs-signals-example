d2c8ca2d23a3ad4120ed10d9907472a57efe719a
Alex Faessen <alex.faessen@marel.com>
2025-02-03 22:15:35 +0100

    Added cart totals

--- apm/src/app/cart/cart-total/cart-total.component.ts
+++ apm/src/app/cart/cart-total/cart-total.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgIf, CurrencyPipe } from '@angular/common';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'sw-cart-total',
@@ -8,12 +9,13 @@ import { NgIf, CurrencyPipe } from '@angular/common';
   imports: [NgIf, CurrencyPipe]
 })
 export class CartTotalComponent {
-  // Just enough here for the template to compile
-  cartItems = [];
-
-  subTotal = 100;
-  deliveryFee = 20;
-  tax = 10;
-  totalPrice = this.subTotal + this.deliveryFee + this.tax;
+  
+  private cardService = inject(CartService);
+  
+  cartItems = this.cardService.cartItems;
+  subTotal = this.cardService.subTotal;
+  deliveryFee = this.cardService.deliveryFee;
+  tax = this.cardService.tax;
+  totalPrice = this.cardService.totalPrice;
 
 }
--- apm/src/app/cart/cart.service.ts
+++ apm/src/app/cart/cart.service.ts
@@ -13,10 +13,13 @@ export class CartService {
   cartCount = computed(() => this.cartItems()
     .reduce((accQty, item) => accQty + item.quantity, 0));
 
-
+  // Cart total calculations
+  subTotal = computed(() => this.cartItems().reduce((accTotal, item ) => accTotal + (item.product.price * item.quantity), 0));
+  deliveryFee = computed<number> (() => this.subTotal() < 50 ? 5.99 : 0);
+  tax = computed(() => Math.round(this.subTotal() * 10.75) / 100);
+  totalPrice = computed(() => this.subTotal() + this.deliveryFee() + this.tax());
 
   eLength = effect(() => console.log('Cart array length', this.cartItems().length));
-
 
   addToCart(product: Product): void {
 
